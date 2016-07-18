7600764ed246f14c48c53403c3413eed34a4dedd
zhoutl <18551859864@163.com>
2016-07-18 23:18:21 +0800

    1111111111

--- ThinkPHP/Library/Vendor/Code/Phpqrcode.php
+++ ThinkPHP/Library/Vendor/Code/Phpqrcode.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
  * PHP QR Code encoder
  *
@@ -31,24 +32,15 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  */
-
-
+ 
+ 
 
 /*
  * Version: 1.1.4
  * Build: 2010100721
  */
 
-/*
- * 其中参数$text表示生成二位的的信息文本；
- * 参数$outfile表示是否输出二维码图片 文件，默认否；
- * 参数$level表示容错率，也就是有被覆盖的区域还能识别，分别是 L（QR_ECLEVEL_L，7%），M（QR_ECLEVEL_M，15%），Q（QR_ECLEVEL_Q，25%），H（QR_ECLEVEL_H，30%）；
- *  参数$size表示生成图片大小，默认是3；
- * 参数$margin表示二维码周围边框空白区域间距值；
- * 参数$saveandprint表示是否保存二维码并 显示。
- *
- *
- * */
+
 
 //---- qrconst.php -----------------------------
 
